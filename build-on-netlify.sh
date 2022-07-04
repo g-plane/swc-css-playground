3afe4b266aa52f460166b099e81cb811af70eb73
Pig Fang <g-plane@hotmail.com>
2022-07-04 23:27:56 +0800

    fix Deno bin path on Netlify

--- build-on-netlify.sh
+++ build-on-netlify.sh
@@ -1,5 +1,7 @@
 set -ex
-curl -fsSL https://deno.land/install.sh | sh
 rustup toolchain install stable
-/opt/buildhome/.deno/bin/deno task wasmbuild
+curl -fsSL https://deno.land/install.sh | sh
+export DENO_INSTALL="/opt/buildhome/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+deno task wasmbuild
 npm run build
