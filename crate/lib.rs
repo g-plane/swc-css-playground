94894070826aba3353e4d8922eb69800f6728f1b
Pig Fang <g-plane@hotmail.com>
2022-07-04 23:47:37 +0800

    mark errors recoverable

--- crate/lib.rs
+++ crate/lib.rs
@@ -38,19 +38,25 @@ pub fn parse(code: String, allow_wrong_line_comments: bool) -> Result<JsValue, J
     ) {
         Ok(stylesheet) => ParserResult {
             ast: Some(stylesheet),
-            errors: errors.into_iter().map(convert_error).collect(),
+            errors: errors.into_iter().map(convert_recoverable_error).collect(),
         },
-        Err(error) => ParserResult {
-            ast: None,
-            errors: vec![convert_error(error)],
-        },
+        Err(error) => {
+            let message = error.message().to_string();
+            ParserResult {
+                ast: None,
+                errors: vec![Error {
+                    span: error.into_inner().0,
+                    message,
+                }],
+            }
+        }
     };
 
     serde_wasm_bindgen::to_value(&result).map_err(JsError::from)
 }
 
-fn convert_error(error: swc_css_parser::error::Error) -> Error {
-    let message = error.message().to_string();
+fn convert_recoverable_error(error: swc_css_parser::error::Error) -> Error {
+    let message = format!("{} (Recoverable)", error.message());
     Error {
         span: error.into_inner().0,
         message,
