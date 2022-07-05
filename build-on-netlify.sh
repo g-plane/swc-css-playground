4b9016f4545497d679845d7bbaea39020acfcad0
Pig Fang <g-plane@hotmail.com>
2022-07-05 14:09:18 +0800

    upgrade `wasmbuild`

--- build-on-netlify.sh
+++ build-on-netlify.sh
@@ -1,7 +1,5 @@
 set -ex
 rustup toolchain install stable
 curl -fsSL https://deno.land/install.sh | sh
-export DENO_INSTALL="/opt/buildhome/.deno"
-export PATH="$DENO_INSTALL/bin:$PATH"
-deno task wasmbuild
+/opt/buildhome/.deno/bin/deno task wasmbuild
 npm run build
